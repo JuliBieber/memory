e40a232fd16a5971ceda008f932cf83b2db44cb6
Christian Hey <c.hey@sbs-herzogenaurach.de>
2023-09-21 08:32:42 +0200

    Added timer for card flipping

--- MyLabel.py
+++ MyLabel.py
@@ -1,4 +1,4 @@
-from PyQt6.QtCore import QSize
+from PyQt6.QtCore import QSize, QTimer, pyqtSlot
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QLabel
 
@@ -8,7 +8,7 @@ class MyLabel(QLabel):
         super().__init__(parent)
 
         self.__image_name = image_name
-        self.__is_turned = False
+        self.__is_turned = True
 
         pixmap_size = QSize(200, 200)
 
@@ -17,10 +17,21 @@ class MyLabel(QLabel):
 
         self.setPixmap(self.__icon_background)
 
+        self.__timer = QTimer()
+        self.__timer.timeout.connect(self.turn_card)
+        self.__timer.setSingleShot(True)
+
     def mousePressEvent(self, ev) -> None:
         if self.__is_turned:
-            self.setPixmap(self.__icon_background)
-            self.__is_turned = False
-        else:
             self.setPixmap(self.__icon_symbol)
+            self.__is_turned = False
+
+            self.__timer.start(3 * 1000)
+
+    @pyqtSlot()
+    def turn_card(self):
+        print("got a call")
+
+        if not self.__is_turned:
+            self.setPixmap(self.__icon_background)
             self.__is_turned = True
